52e2a5fd724d75d0e87174eef995d4514d25e9d7
mdaxc6 <mikeabsher1128@gmail.com>
2021-05-19 18:03:26 -0500

    added functions for bubble chart and metadata

--- static/js/app.js
+++ static/js/app.js
@@ -7,30 +7,38 @@ d3.json("../plotly-challenge/samples.json").then(function(samples){
     init(samples);
 });
 
-// numArray.sort(function compareFunction(firstNum, secondNum) {
-//     // resulting order is (3, 2, 1)
-//     return secondNum - firstNum;
-//   });
-
-function plotData(samples){
+function getSample(samples){
     var selectNode = d3.select("#selDataset");
-
     var sampleID = selectNode.property("value");
-
     var sampleData = samples["samples"].filter(sample => sample.id == sampleID);
+    return sampleData;
+}
 
+function getMeta(samples){
+    var selectNode = d3.select("#selDataset");
+    var sampleID = selectNode.property("value");
+    var metadata = samples["metadata"].filter(sample => sample.id == sampleID);
+    
+    var metaDiv = d3.select("#sample-metadata");
+    console.log(metadata[0])
+    Object.entries(metadata[0]).forEach(([key, value]) => {
+        metaDiv.append("p")
+            .html(`<b>${key}: ${value}</b>`);
+    });
+}
+
+function plotBar(samples){
+
+    var sampleData = getSample(samples);
+
+    // add labels to y data
     var yData = sampleData[0].otu_ids.slice(0,10);
     var yLabels = [];
-
-    console.log(yData)
-
     yData.forEach(function(sample){
         yLabels.push(`OTU ${sample.toString()}`);
     });
 
-    console.log(yLabels)
-
-    var trace = {
+    var barTrace = {
         x: sampleData[0].sample_values.slice(0,10).reverse(),
         y: yLabels.reverse(),
         text: sampleData[0].otu_labels.slice(0,10),
@@ -38,15 +46,41 @@ function plotData(samples){
         orientation: "h"
     };
 
-    var chartData = [trace];
+    var barData = [barTrace];
 
-    return chartData;
+    return barData;
+}
+
+function plotBubble(samples){
+    var sampleData = getSample(samples);
+
+    var bubbleTrace = {
+        x: sampleData[0].otu_ids,
+        y: sampleData[0].sample_values,
+        mode: 'markers',
+        marker: {
+            size: sampleData[0].sample_values,
+            color: sampleData[0].otu_ids,
+        },
+        text: sampleData[0].otu_labels
+
+    };
+    
+    var bubbleData = [bubbleTrace];
+
+    return bubbleData;
+    
 }
 
 
 function init(samples){
-    var chartData = plotData(samples);
-    Plotly.newPlot("bar", chartData);
+    var barData = plotBar(samples);
+    Plotly.newPlot("bar", barData);
+
+    var bubbleData = plotBubble(samples);
+    Plotly.newPlot("bubble", bubbleData);
+
+    getMeta(samples);
 }
 
 
